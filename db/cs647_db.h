94cbb56839c367b2fdaad57b3dea834c6364aaab
manosanaggh <manos.anagnostakis@windowslive.com>
2022-10-24 11:20:52 +0300

    [MOD] Added helpful comments in db header from YCSB wiki.

--- db/cs647_db.h
+++ db/cs647_db.h
@@ -18,16 +18,25 @@ namespace ycsbc {
 
 class CS647DB : public DB {
  public:
+  //Read a single record
   int Read(const std::string &table, const std::string &key,
            const std::vector<std::string> *fields,
            std::vector<KVPair> &result);
+
+  //Perform a range scan
   int Scan(const std::string &table, const std::string &key,
            int len, const std::vector<std::string> *fields,
            std::vector<std::vector<KVPair>> &result);
+
+  //Update a single record
   int Update(const std::string &table, const std::string &key,
              std::vector<KVPair> &values);
+
+  //Insert a single record
   int Insert(const std::string &table, const std::string &key,
              std::vector<KVPair> &values);
+
+  //Delete a single record
   int Delete(const std::string &table, const std::string &key);
 };
 
